f8d067091015f9a45e478730a17753dd6e79413e
Farid <farid@MacBook-Pro-Farid.local>
2023-08-21 12:31:49 +0400

    fixed error

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./styles.module.css";
+import Link from "next/link";
 
 function Support() {
   const [formData, setFormData] = useState({
